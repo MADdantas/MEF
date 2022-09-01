50d779016ef2d9f13c423649883c083caea09101
MADdantas <matheussdantas19@gmail.com>
2022-09-01 17:05:06 -0300

    Added displacements results

--- trelica.py
+++ trelica.py
@@ -35,41 +35,41 @@ import math
 ###################################
 
 # Matriz de rigidez das barras no sistema local, kb
-def f_kb(EA,L):
-    kb = np.zeros((1,4))
+def f_kb(EA,L,n):
+    kb = np.zeros((n,1))
     for i in range(len(EA)):
-        kb = np.concatenate((kb, ((EA[i]/L[i] * np.array([[1, 0, -1, 0], 
+        kb = np.hstack((kb, ((EA[i]/L[i] * np.array([[1, 0, -1, 0], 
                                                             [0, 0, 0, 0], 
                                                             [-1, 0, 1, 0], 
                                                             [0, 0, 0, 0]])))))
-    kb = kb[1:].T
+    kb = kb[:,1:]
     return kb
 
 # Matriz de transformação das barras, T
-def f_t(theta):
-    T = np.zeros((1,4))
+def f_t(theta,n,e):
+    T = np.zeros((n,1))
     for i in range(len(theta)):
-        T = np.concatenate((T, 1*(np.array([[math.cos(theta[i]*math.pi/180), math.sin(theta[i]*math.pi/180), 0, 0], 
-                                            [-math.sin(theta[i]*math.pi/180), math.cos(theta[i]*math.pi/180), 0, 0], 
-                                            [0, 0, math.cos(theta[i]*math.pi/180), math.sin(theta[i]*math.pi/180)], 
-                                            [0, 0, -math.sin(theta[i]*math.pi/180), math.cos(theta[i]*math.pi/180)]]))))
-    T = T[1:].T
+        T = np.hstack((T, (np.array([[math.cos(theta[i]*(math.pi/180)), math.sin(theta[i]*(math.pi/180)), 0, 0], 
+                                          [-math.sin(theta[i]*(math.pi/180)), math.cos(theta[i]*(math.pi/180)), 0, 0], 
+                                          [0, 0, math.cos(theta[i]*(math.pi/180)), math.sin(theta[i]*(math.pi/180))], 
+                                          [0, 0, -math.sin(theta[i]*(math.pi/180)), math.cos(theta[i]*(math.pi/180))]]))))
+    T = T[:,1:]
     return T
 
 # Matriz de rigidez da barra no sistema global, k
-def f_k(kb: np.array, T: np.array, e: int):
-    k = np.zeros((1,4))
+def f_k(kb: np.array, T: np.array, e: int, n: int):
+    k = np.zeros((n,1))
 
     for i in range(e):
-        _t = T[:,i*4:(i+1)*4].T
-        kb_ = kb[:,i*4:(i+1)*4]
-        t_ = T[:,i*4:(i+1)*4]
-        k = np.concatenate((k, np.matmul(_t,np.matmul(kb_,t_))))
-    k = k[1:].T
+        _t = T[:,i*n:(i+1)*n].T
+        kb_ = kb[:,i*n:(i+1)*n]
+        t_ = T[:,i*n:(i+1)*n]
+        k = np.hstack((k, np.matmul(_t,np.matmul(kb_,t_))))
+    k = k[:,1:]
     return k
 
 # Matriz de incidência, Lb
-def f_lb(e,n,b):
+def f_Lb(e,n,b):
     Lb = np.zeros((n*2))
     for c in range(e):
         for i in  range(n*2):
@@ -80,6 +80,24 @@ def f_lb(e,n,b):
     Lb = Lb[n*2:]
     Lb = Lb.reshape((e*n,n*2))
     return Lb
+
+# Matriz de rigidez da estrutura, K
+def f_K(Lb: np.array, k: np.array, e: int, n: int):
+    K = np.zeros((Lb.shape[1], Lb.shape[1]))
+    for i in range(e):
+        K = K + np.matmul(Lb[i*n:(i*n)+n,:].T, np.matmul(k[:,i*n:(i*n)+n], Lb[i*n:(i*n)+n,:]))
+    return K
+
+# Matriz de rigidez reduzida da estrutura, Kaa
+def f_Kaa(K: np.array,Fe: np.array):
+    l = len(Fe)
+    Kaa = K[0:l,0:l]
+    return Kaa
+
+# Deslocamentos nodais global, U [m]
+def f_U(Fe,Kaa,n):
+    U = np.concatenate((np.matmul(np.linalg.inv(Kaa), Fe),np.zeros((2*n-len(Fe))))).reshape((2*n,1))
+    return U
 
 #######################
 # Calculo de trelicas #
@@ -98,7 +116,11 @@ theta: np.array, n: int, b: np.array, Fe: np.array, EI, P):
         print("at least 6 input arguments required")
         return
     e = L.shape[0] # Número de barras
-    kb = f_kb(EA,L)
-    T = f_t(theta)
-    k = f_k(kb,T,e)
-    Lb = f_lb(e,n,b)
+    kb = f_kb(EA,L,n)
+    T = f_t(theta,n,e)
+    k = f_k(kb,T,e,n)
+    Lb = f_Lb(e,n,b)
+    K = f_K(Lb,k,e,n)
+    Kaa = f_Kaa(K,Fe)
+    U = f_U(Fe,Kaa,n)
+    print(U)
